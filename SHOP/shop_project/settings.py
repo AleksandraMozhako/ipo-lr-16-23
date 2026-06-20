6c794eb7080d6f57e1cccb2e33f611ed60f084c1
Aleksandra <mozhakoaleksandra@gmail.com>
2026-06-20 22:09:25 +0300

    lr18

--- SHOP/shop_project/settings.py
+++ SHOP/shop_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shop_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,3 +116,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Настройки аутентификации
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Медиа файлы
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
